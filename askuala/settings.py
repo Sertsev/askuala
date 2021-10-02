117b3b296f07048c5cdbef5570e3ae3b1c6a055b
sertsev <sertseshewa@gmail.com>
2021-10-02 06:54:01 +0300

    hiding secret key

--- askuala/settings.py
+++ askuala/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'src')
+STATIC_ROOT = os.path.join(BASE_DIR, 'src')
 
 STATICFILES_DIRS = [
     STATIC_DIR,
