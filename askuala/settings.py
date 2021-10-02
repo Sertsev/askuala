f67c050b763f971b4b7e371dd78a0f5baf0a6347
sertsev <sertseshewa@gmail.com>
2021-10-02 06:26:43 +0300

    hiding secret key

--- askuala/settings.py
+++ askuala/settings.py
@@ -24,7 +24,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'src')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config("SECRET_KEY") 
+SECRET_KEY = Config("SECRET_KEY") 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
